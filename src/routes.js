8646aaa76feeb0883f27a611948f2b6a5ef3b258
Tomas Giacaman <tomas@ormasoft.cl>
2018-02-20 16:22:10 -0300

    Borrado

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-import pantallas from 'pantallas/routes';
-
-const routes = [
-  { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: false, title: 'inicio' },
-  ...pantallas,
-];
-
-export default routes;
--- src/services/backend.js
+++
@@ -1,8 +0,0 @@
-const latency = 0;
-
-function wait(ms = latency) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-export class Backend {
-}
