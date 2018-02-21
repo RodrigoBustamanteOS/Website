93c6bd6b713c93fad153a9299fea33b08b0abadf
Rodrigo Bustamante <rodrigo@ormasoft.cl>
2018-02-21 13:06:58 -0300

    Added contact form component

---
+++ src/components/contact-form/contact-form.js
@@ -0,0 +1,11 @@
+export class ContactFormCustomElement {
+    constructor() {
+        this.name;
+        this.email
+        this.message
+    }
+
+    sendMessage() {
+        console.log("test")
+    }
+}
