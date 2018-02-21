cdd0aed0d01dba29830c81a1457656f59078cd2c
Jaime Torres <jaime@ormasoft.cl>
2018-02-21 16:27:22 -0300

    nav example

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,8 @@ import pantallas from 'pantallas/routes';
 
 const routes = [
   { route: ['', 'welcome'], name: 'welcome', moduleId: 'welcome', nav: false, title: 'inicio' },
-  ...pantallas,
+  { route: '/blog', name: 'blog', moduleId: 'blog', nav: true, title: 'blog' },
+  ...pantallas
 ];
 
 export default routes;
