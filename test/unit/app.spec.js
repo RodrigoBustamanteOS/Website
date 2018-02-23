5a243026318831ac68bdb02bf9adcea6317bf5a8
Rodrigo Bustamante <rodrigo@ormasoft.cl>
2018-02-23 13:21:11 -0300

    Updates

--- test/unit/app.spec.js
+++
@@ -1,7 +0,0 @@
-import {App} from '../../src/app';
-
-describe('the app', () => {
-  it('says rayen', () => {
-    expect(new App().title).toBe('rayen');
-  });
-});
--- test/unit/setup.js
+++
@@ -1,6 +0,0 @@
-import 'aurelia-polyfills';
-import {initialize} from 'aurelia-pal-browser';
-// import $ from 'jquery';
-// import 'bootstrap-datepicker';
-// import bdpLocale from 'patch/bootstrap-datepicker/locale/bootstrap-datepicker.es';
-initialize();
