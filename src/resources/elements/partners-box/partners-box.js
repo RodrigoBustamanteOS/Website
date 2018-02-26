513c953303a4e8bbf0411f84fe24902d8a910742
Rodrigo Bustamante <rodrigo@ormasoft.cl>
2018-02-26 09:24:24 -0300

    Changes from viviana's branch

--- src/resources/elements/partners-box/partners-box.js
+++ src/resources/elements/partners-box/partners-box.js
@@ -1,10 +1,9 @@
-export class PartnersBox{
-  constructor(){
-    this.people = [
-      { name: 'Lorem, ipsum.', image: 'https://picsum.photos/250/100/?random', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, quia magnam perferendis dolores nesciunt exercitationem nostrum sit ut aspernatur maxime repellat distinctio! Ad assumenda atque vero magni ducimus voluptas nemo, amet voluptatibus a voluptates earum provident.'},
-      { name: 'Lorem, ipsum.', image: 'https://picsum.photos/250/100/?random', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, quia magnam perferendis dolores nesciunt exercitationem nostrum sit ut aspernatur maxime repellat distinctio! Ad assumenda atque vero magni ducimus voluptas nemo, amet voluptatibus a voluptates earum provident.'},
-      { name: 'Lorem, ipsum.', image: 'https://picsum.photos/250/100/?random', description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, quia magnam perferendis dolores nesciunt exercitationem nostrum sit ut aspernatur maxime repellat distinctio! Ad assumenda atque vero magni ducimus voluptas nemo, amet voluptatibus a voluptates earum provident.'}
-    ]
-  }
+import {bindable, bindingMode} from 'aurelia-framework';
+export class PartnersBox {
+    @bindable name = "";
+    @bindable description = "";
+    @bindable img = "";
+    constructor() {
+        
+    }
 }
-
